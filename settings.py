fc529c5adba2af39508b639db6a745c74743f4c3
len1598 <37744306+len1598@users.noreply.github.com>
2018-07-06 17:16:36 +0800

    Update and rename Setting.py to settings.py

---
+++ settings.py
@@ -0,0 +1,6 @@
+class Settings():
+    def __init__(self):
+        self.sreen_width = 800
+        self.sreen_height = 600
+        self.bg_color = (100,130,230)
+        self.ship_speed_factor = 1.5
